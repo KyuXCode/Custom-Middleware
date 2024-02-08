56b0fa9d969f546224cc564409480adee7243da2
Kevin-Yu <kai38547@gmail.com>
2024-02-07 21:21:58 -0500

    Added test

---
+++ Web.Tests/MyAuthTests.cs
@@ -0,0 +1,53 @@
+using System.Net;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using Web.Middleware;
+
+namespace Middleware.Tests;
+
+public class MyAuthTests : IAsyncLifetime
+{
+    IHost? host;
+
+    public Task DisposeAsync()
+    {
+        return Task.CompletedTask;
+    }
+
+    public async Task InitializeAsync()
+    {
+        host = await new HostBuilder()
+            .ConfigureWebHost(webBuilder =>
+            {
+                webBuilder
+                    .UseTestServer()
+                    .ConfigureServices(services => { })
+                    .Configure(app =>
+                    {
+                        app.UseMyMiddleware();
+                        app.Run(async context => { await context.Response.WriteAsync("Authenticated!"); });
+                    });
+            })
+            .StartAsync();
+    }
+
+    [Fact]
+    public async Task MiddlewareTest_FailWhenNotAuthenticated()
+    {
+        var response = await host.GetTestClient().GetAsync("/");
+        Assert.NotEqual(HttpStatusCode.NotFound, response.StatusCode);
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Not Authorized!", result);
+    }
+
+    [Fact]
+    public async Task MiddlewareTest_Authenticated()
+    {
+        var response = await host.GetTestClient().GetAsync("/?username=user1&password=password1");
+        var result = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Authenticated!", result);
+    }
+}
--- Web/Middleware/MyMiddleware.cs
+++ Web/Middleware/MyMiddleware.cs
@@ -2,18 +2,23 @@ namespace Web.Middleware
 {
     public class MyMiddleware
     {
-        private readonly RequestDelegate _next;
-        public MyMiddleware(RequestDelegate next) {
+        private readonly RequestDelegate _next; 
+        
+        public MyMiddleware(RequestDelegate next) 
+        {
             _next = next;
         }
-        public async Task InvokeAsync(HttpContext context) {
+        
+        public async Task InvokeAsync(HttpContext context) 
+        {
             if (context.Request.Query["username"] == "user1" && context.Request.Query["password"] == "password1") {
                 context.Request.HttpContext.Items.Add("userdetails", "blallf");
                 await _next(context);
-
-            } else {
+            } 
+            else 
+            {
                 await context.Response.WriteAsync("Not Authorized!");
-            };
+            }
         }
     }
 }
